0fb2e4ac6a57014c752a1da30fab85ce54348ebf
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-10-21 19:40:21 -0300

    feat(server settings): server created

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Funcionou!")
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config()
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+server.get('/ping', (req, res) => {
+    res.json('pong')
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
