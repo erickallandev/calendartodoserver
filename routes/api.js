a1274550f08c79bae3eb38b1c5a862919412a655
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-10-22 01:15:22 -0300

    fix(BASE_URL): changed base_url to heroku's link

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,30 @@
 import {Router} from 'express';
 import bodyParser from 'body-parser';
 import * as Controllers from '../controllers/todoController.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = Router();
 const urlencodedParser = bodyParser.urlencoded({
     extended: false
 })
 
+const BASEURL = process.env.BASEURL;
+
 //Get all the to-do's
-router.get('/todos', Controllers.getAllTodos);
+router.get(`${BASEURL}/todos`, Controllers.getAllTodos);
 
 //Get one of the to-do's by ID
-router.get('/todos/:id', Controllers.getTodoById );
+router.get(`${BASEURL}/todos/:id`, Controllers.getTodoById );
 
 //Add a new to-do
-router.post('/todos', urlencodedParser, Controllers.addNewTodo);
+router.post(`${BASEURL}/todos`, urlencodedParser, Controllers.addNewTodo);
 
 //Edit one of the to-do's by ID
-router.put('/todos/:id', urlencodedParser, Controllers.editTodo);
+router.put(`${BASEURL}/todos/:id`, urlencodedParser, Controllers.editTodo);
 
 //Delete a to-do by id
-router.delete('/todos/:id', Controllers.deleteTodo);
+router.delete(`${BASEURL}/todos/:id`, Controllers.deleteTodo);
 
 export default router
